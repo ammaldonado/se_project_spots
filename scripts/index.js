dd57741d29e98c87ab732388266d6b5010268454
Abel Maldonado <a.m.maldonado@hotmail.com>
2025-01-11 08:44:26 -0600

    gitupdatestage4

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialcards = [
+const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
@@ -27,14 +27,63 @@ const initialcards = [
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
 
+const profileName = document.querySelector(".profile__name");
+const profileDiscription = document.querySelector(".profile__discription");
+
 const editProfileModal = document.querySelector("#edit-modal");
+const editFormElement = editProfileModal.querySelector(".modal__form");
 
 const editModalCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-profileEditButton.addEventListener("click", function () {
-  editProfileModal.classList.add("modal__open");
-});
+const editModalNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editModalDiscriptionInput = editProfileModal.querySelector(
+  "#profile-discription-input"
+);
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
 
-editModalCloseBtn.addEventListener("click", function () {
+function getCardElement(data) {
+  const cardElement = document
+    .querySelector("#card-template")
+    .content.querySelector(".card")
+    .cloneNode(true);
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardLinkEl = cardElement.querySelector(".card__image");
+  cardNameEl.textContent = data.name;
+  cardLinkEl.alt = data.name;
+
+  cardLinkEl.src = data.link;
+
+  return cardElement;
+}
+
+function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDiscriptionInput.value = profileDiscription.textContent;
+  editProfileModal.classList.add("modal__open");
+}
+
+function closeModal() {
   editProfileModal.classList.remove("modal__open");
-});
+}
+
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDiscription.textContent = editModalDiscriptionInput.value;
+  closeModal();
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseBtn.addEventListener("click", closeModal);
+
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  console.log(cardElement);
+  cardsList.prepend(cardElement);
+}
